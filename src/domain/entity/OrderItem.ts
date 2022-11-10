503cabdae68bbf996f8f742e15f974088f22abb6
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2022-11-10 08:33:52 -0300

    implementaçao do order repository database

--- src/domain/entity/OrderItem.ts
+++ src/domain/entity/OrderItem.ts
@@ -1,8 +1,9 @@
+import Dimension from "./Dimension";
 import Item from "./Item";
 
 export default class OrderItem {
 
-  constructor(readonly idItem: number, readonly price: number, readonly quantity: number) {
+  constructor(readonly idItem: number, readonly price: number, readonly quantity: number, readonly dimensions?: Dimension) {
     if(quantity < 0) throw new Error("Invalid quantity")
   }
 
