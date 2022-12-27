69ee3ac86fa49236e103d50d86dd804471167efc
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2022-12-27 09:11:54 -0300

    iniciando frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+  import axios from "axios";
+  import { reactive } from "vue";
+
+  const state = reactive({
+    items: [],
+    order: {
+      cpf: "",
+      orderItems: []
+    }
+  })
+
+  function addItem(item: any) {
+    const existingOrderItem = state.order.orderItems.find((orderItem: any) => orderItem.idItem === item.idItem)
+    if(existingOrderItem) {
+      existingOrderItem.quantity++
+    } else {
+      state.order.orderItems.push({ idItem: item.idItem, quantity: 1 })
+    }
+  }
+
+  function removeOrderItem(orderItem: any) {
+    const existingOrderItem = state.order.orderItems.find((order: any) => order.idItem === orderItem.idItem)
+    if(existingOrderItem) {
+      existingOrderItem.quantity--
+      if(existingOrderItem.quantity == 0) {
+        state.order.orderItems.splice(state.order.orderItems.indexOf(existingOrderItem), 1)
+      }
+    }
+  }
+
+  axios.get("http://localhost:3002/items").then(function(response) {
+    state.items = response.data
+  });
+
+</script>
+
+<template>
+  <div v-for="item in state.items">
+    {{item.description}}
+    {{item.price}}
+    <button @click="addItem(item)">Add</button>
+  </div>
+
+  <div v-for="orderItem in state.order.orderItem">
+    {{orderItem.idItem}}
+    {{orderItem.quantity}}
+    <button @click="removeOrderItem(orderItem)">-</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
