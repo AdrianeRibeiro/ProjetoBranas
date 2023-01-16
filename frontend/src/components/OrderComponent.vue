d1d001c29347bc614d2520c4408be3052ba7af9d
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2023-01-16 08:49:20 -0300

    testes automatizados do frontend

--- frontend/src/components/OrderComponent.vue
+++ frontend/src/components/OrderComponent.vue
@@ -15,6 +15,6 @@
   <label>coupon</label>
   <input type="text" v-model="order.coupon" @blur="$emit('validateCoupon', order.coupon)"/>
 
-  {{total}}
+  <div class="order-total">{{total}}</div>
   <button @click="$emit('checkout', order)">Checkout</button>
 </template>
